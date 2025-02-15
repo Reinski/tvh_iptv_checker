f0bcaf4faf7869a0db69b5b75e8d8f5c1afd4325
Reiner Nikulski <info@nikulski-it.de>
2025-02-15 12:06:31 +0100

    fix env issue

--- run_tvh_checker.sh
+++ run_tvh_checker.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 source /home/pi/.bash_tvh
 python3 ~/check_m3u.py -s 127.0.0.1:9981 -user "${TVH_USER}" -pass "${TVH_PASS}" --smtp-server "${SMTP_SERVER}" --smtp-port "${SMTP_PORT}" --sender-email "${SMTP_SENDER}" --recipient-email ""${SMTP_RECIPIENT}"" --smtp-username "${SMTP_USER}" --smtp-password "${SMTP_PASS}"
